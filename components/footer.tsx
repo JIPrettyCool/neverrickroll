9cb554d0c98b0a99f0654b2310246ae5a32aa1e7
JI <49317513+JIPrettyCool@users.noreply.github.com>
2021-04-20 11:50:44 +0300

    Dumb JI

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,6 @@ const ExternalLink = ({ children }) => (
     className="text-gray-500 hover:text-gray-600 transition"
     target="_blank"
     rel="noopener noreferrer"
-    href={href}
   >
     {children}
   </a>
